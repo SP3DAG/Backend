13b21aefbc2d95afd73ebfdeebc2e7f924bf0240
denkmoritz <moritzdenk0407@gmail.com>
2025-06-28 12:41:40 +0200

    qr code 51 fix

--- decoding/decoding.py
+++ decoding/decoding.py
@@ -31,7 +31,7 @@ def extract_qr_and_signature_at(
     offset_x: int,
     offset_y: int,
     block_size: int = 8,
-    qr_size: int = 49
+    qr_size: int = 51
 ) -> Tuple[
         List[List[int]],   # qr_matrix
         bytes,             # sig_input (bitmap‖deviceID‖totalTiles‖qr_index)
@@ -148,7 +148,7 @@ def decode_qr_image_opencv(qr_img_pil: Image.Image) -> str:
 def decode_all_qr_codes(image_path: str,
                         block_size: int = 8,
                         spacing_px: int = 20,
-                        qr_size: int = 49) -> List[Dict[str, str]]:
+                        qr_size: int = 51) -> List[Dict[str, str]]:
     """
     Scans the whole image, verifies every tile, and returns:
 
